e8ee48098a13ca21af42e935905be1b07b8e9953
jigi <ahraeha@naver.com>
2023-01-19 04:25:24 +0900

    Initial commit

---
+++ src/main/java/com/jjez/commboard/CommboardApplication.java
@@ -0,0 +1,13 @@
+package com.jjez.commboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjez/commboard/CommboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjez.commboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
